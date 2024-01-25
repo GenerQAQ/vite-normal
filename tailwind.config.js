f4510bb0bcbea0d89ad3725c8479a1e0bc7cd82b
Gener <39689863+GenerQAQ@users.noreply.github.com>
2024-01-25 10:31:45 +0000

    fix: up package

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     important: true,
-    content: ['./src/projects/**/*.{html,vue,js,ts,jsx,tsx}', './scripts/template/**/*.{html,vue,js,ts,jsx,tsx}'],
+    content: ['./src/**/*.{html,vue,js,ts,jsx,tsx}'],
     theme: {
         extend: {}
     },
