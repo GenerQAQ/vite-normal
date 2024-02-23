4c6bad71458b1ac95fc9764593d074a1d2f12c06
Gener <39689863+GenerQAQ@users.noreply.github.com>
2024-02-23 03:49:20 +0000

    fix: style lint error

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -29,7 +29,7 @@ const count = ref(0);
     <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 .read-the-docs {
     color: #888;
 }
